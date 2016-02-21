5498dc8ca494ae74404c894548a86c5a81250bb3
Bruno Araujo <araujo.contato@gmail.com>
2016-02-21 16:38:09 -0300

    Criando pastas, subindo o diagrama de classes e projeto do libGDX vazio.

---
+++ libGDX Project/desktop/src/com/brunoemarcos/typinggame/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.brunoemarcos.typinggame.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.brunoemarcos.typinggame.GameManager;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new GameManager(), config);
+	}
+}
