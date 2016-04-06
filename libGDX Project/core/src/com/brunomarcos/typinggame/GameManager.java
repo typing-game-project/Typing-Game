914f48eb1f832f2d079b463d4dac73d9d5a71325
Bruno Araujo <araujo.contato@gmail.com>
2016-04-06 03:22:34 -0300

    implementação dos assets e fullscreen

--- libGDX Project/core/src/com/brunomarcos/typinggame/GameManager.java
+++ libGDX Project/core/src/com/brunomarcos/typinggame/GameManager.java
@@ -3,32 +3,53 @@ package com.brunomarcos.typinggame;
 import java.util.ArrayList;
 
 import com.badlogic.gdx.Game;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public class GameManager extends Game {
 	public SpriteBatch batch;
-	public BitmapFont font;
+	public BitmapFont fontP2white;
+	public BitmapFont fontP2black;
 	public boolean sfxOn;
 	public boolean bgmOn;
 	public Player player;
 	public ArrayList<Level> levels;
+	public Texture[] bg;
+	public Texture hud;
+	public Texture rect;
+	public int width;
+	public int height;
 	
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
-		font = new BitmapFont();
+		fontP2white = new BitmapFont(Gdx.files.internal("fonts/PressStart2P_white.fnt"));
+		fontP2black = new BitmapFont(Gdx.files.internal("fonts/PressStart2P_black.fnt"));
 		sfxOn = true; // Para usar nas opções
 		bgmOn = true; // Para usar nas opções
 		player = new Player();
 		levels = new ArrayList<Level>();
 		
+		// Salvando tamanho da tela
+		width = Gdx.graphics.getWidth();
+		height = Gdx.graphics.getHeight();
+		
+		// Instânciando as imagens
+		bg = new Texture[3];
+		bg[0] = new Texture(Gdx.files.internal("Red_Background.jpg"));
+		bg[1] = new Texture(Gdx.files.internal("Green_Background.jpg"));
+		bg[2] = new Texture(Gdx.files.internal("Blue_Background.jpg"));
+		hud = new Texture(Gdx.files.internal("HUD.png"));
+		rect = new Texture(Gdx.files.internal("rect.jpg"));
+		
 		// Instânciando os levels:
-		levels.add(0, new Level(this,"teste de ç e á","DEBUG1",0)); // refêrencia ao game, número do level, frase, password, timer
+		levels.add(0, new Level(this,"teste de ç e á.","DEBUG1",0)); // refêrencia ao game, número do level, frase, password, timer
 		levels.add(1, new Level(this,"FRASE FRASE FRASE FRASE FRASE FRASE GRANDE DE EXEMPLO PARA A SEGUNDA TELA, FRASE GRANDE DE EXEMPLO PARA A SEGUNDA TELA","DEBUG2",60));
 		
 		// Indo para a primeira tela:
-		this.setScreen(levels.get(1));
+		this.setScreen(levels.get(0));
 	}
 	
     public void render() {
@@ -37,6 +58,11 @@ public class GameManager extends Game {
 
     public void dispose() {
         batch.dispose();
-        font.dispose();
+        fontP2white.dispose();
+        fontP2black.dispose();
+        for (Texture i:bg)
+        	i.dispose();
+		hud.dispose();
+		rect.dispose();
     }
 }
--- libGDX Project/core/src/com/brunomarcos/typinggame/Level.java
+++ libGDX Project/core/src/com/brunomarcos/typinggame/Level.java
@@ -3,6 +3,7 @@ package com.brunomarcos.typinggame;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
 
 public class Level implements Screen {
 	final GameManager game; // referência necessária para a classe GameManager
@@ -10,11 +11,57 @@ public class Level implements Screen {
 	public int timer;
 	public Frase frase;
 	
+	// BG
+	private Texture bg;
+	private int hudHeight;
+	private int[] bgOffset;
+	private int velocidadeAnimacaoBG;
+	private boolean vaiVoltaBG;
+	
 	public Level(final GameManager game, String frase, String password, int timer) {
 		this.game = game;
 		this.frase = new Frase(frase);
 		this.password = password;
 		this.timer = timer;
+		
+		// BG
+		this.bg = game.bg[0];
+		hudHeight = (game.height/100) * 14; // Altura da hud em porcentagem
+		bgOffset = new int[2];
+		bgOffset[0] = 0;
+		bgOffset[1] = 0;
+		velocidadeAnimacaoBG = 1;
+		vaiVoltaBG = true;
+	}
+	
+	private void animarBG() {
+		int x = bgOffset[0];
+		int y = bgOffset[1];
+		int w = game.width;
+		int h = game.height;
+		int v = velocidadeAnimacaoBG;
+		
+		if (x >= 200 && vaiVoltaBG)
+			vaiVoltaBG = false;
+		
+		else if (x <= 0 && !vaiVoltaBG)
+			vaiVoltaBG = true;
+			
+		if (!vaiVoltaBG)
+			v *= -1;
+			
+		bgOffset[0] += v;
+		bgOffset[1] += v;
+			
+		game.batch.draw(this.bg, x - w, y - h, w, h);
+		game.batch.draw(this.bg, x, y - h, w, h);
+		game.batch.draw(this.bg, x + w, y - h, w, h);		
+		game.batch.draw(this.bg, x - w, y, w, h);
+		game.batch.draw(this.bg, x, y, w, h);
+		game.batch.draw(this.bg, x + w, y, w, h);
+		game.batch.draw(this.bg, x - w, y + h, w, h);
+		game.batch.draw(this.bg, x, y + h, w, h);	
+		game.batch.draw(this.bg, x + w, y + h, w, h);
 	}
 	
 	@Override
@@ -24,16 +71,20 @@ public class Level implements Screen {
 		Gdx.gl.glClearColor(0, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		
+		animarBG();
+		
+		game.batch.draw(game.hud, 0, game.height - hudHeight, game.width, hudHeight);
+		
 		// Loop para imprimir as letras presentes na frase
 		frase.imprimeFrase(game);
 		
-		// Números para Debug
-		game.font.draw(game.batch, "Pontos: " + Integer.toString(game.player.pontos), 500, 284);
-		game.font.draw(game.batch, "x" + Integer.toString(game.player.multiplicador), 600, 284);
-		game.font.draw(game.batch, "Vida: " + Integer.toString(game.player.vida), 700, 284);
-		game.font.draw(game.batch, "Acertos Consecutivos: " + 
-				Integer.toString(game.player.acertosConsecutivos), 500, 234);
+		int topoTexto = game.height - 35;
 		
+		// Números para Debug
+		game.fontP2white.draw(game.batch, "Pts:" + Integer.toString(game.player.pontos), game.width - 465, topoTexto);
+		game.fontP2white.draw(game.batch, "x" + Integer.toString(game.player.multiplicador), game.width - 600, topoTexto);
+		game.fontP2white.draw(game.batch, "Vida: " + Integer.toString(game.player.vida),  40, topoTexto);
+
 		if (game.player.vida == 0) {
 			//TODO Chamar a tela de DERROTA
 			this.frase.frase.replace(0, frase.frase.length(), frase.fraseCompleta);
